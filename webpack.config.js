0031706e2b83fbf622015e42f6982b010afc31f1
Sergey <sergeylabetsik@datateer.com>
2023-07-02 10:49:47 -0500

    Adjust package.json and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "production",
@@ -22,6 +23,7 @@ module.exports = {
     ],
   },
   externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   optimization: {
     minimize: true,
   },
